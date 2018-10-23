bc9f82b6eb5f95aa92cf01710e7f496a34506de1
Francisco Javier Vadillo <32244264+FranVadillo@users.noreply.github.com>
2018-10-24 01:47:49 +0200

    La nueva clase player para poder incluir varios
    La meto en src aunque igual debería ir en scripts

---
+++ src/player.js
@@ -0,0 +1,49 @@
+class Player{
+    constructor(numberOfPlayer){
+
+        this.speed = 6;
+        this.friction = 0.5;
+        this.score = 0;
+        
+        if(numberOfPlayer == 1){
+            this.sprite = game.add.sprite(0, 0, 'harry');
+            
+            this.upKey = game.input.keyboard.addKey(Phaser.Keyboard.W);
+            this.downKey = game.input.keyboard.addKey(Phaser.Keyboard.S);
+            this.leftKey = game.input.keyboard.addKey(Phaser.Keyboard.A);
+            this.rightKey = game.input.keyboard.addKey(Phaser.Keyboard.D);
+
+        }else{
+            this.sprite = game.add.sprite(0, 0, 'draco');
+            
+            this.upKey = game.input.keyboard.addKey(Phaser.Keyboard.UP);
+            this.downKey = game.input.keyboard.addKey(Phaser.Keyboard.DOWN);
+            this.leftKey = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
+            this.rightKey = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
+
+        }
+
+        this.sprite.scale.setTo(0.5,0.5);
+        this.sprite.anchor.x = 0.5;
+        this.sprite.anchor.y = 0.5;
+
+        game.physics.enable(this.sprite, Phaser.Physics.ARCADE); 
+        this.sprite.body.collideWorldBounds = true;
+    }
+    
+    update(){
+        if (this.upKey.isDown) {
+            this.sprite.y -= this.speed;
+        }
+        else if (this.downKey.isDown) {
+            this.sprite.y += this.speed;
+        }
+
+        if (this.leftKey.isDown) {
+            this.sprite.x -= this.speed;
+        }
+        else if (this.rightKey.isDown) {
+            this.sprite.x += this.speed;
+        }
+    }
+}
