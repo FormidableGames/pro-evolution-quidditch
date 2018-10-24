ee95d90e374c2934d5bb37b72f1c658917326101
svalverdet <32246322+svalverdet@users.noreply.github.com>
2018-10-24 02:56:41 +0200

    Apaño de la animación de las snitches
    Iban muy despacio.

--- src/states/intro.js
+++ src/states/intro.js
@@ -21,16 +21,15 @@ ProEvolutionQuidditch.introState.prototype = {
         
         this.background = this.add.tileSprite(0, 0, 800, 600, 'background1');
         
-        // 10 snitches in the background (left to right)
+        // 10 snitches in the background
         for (var i = 0; i < 10; i++)
         {
             this.spr_snitch = this.add.sprite(-150, -175 + (this.world.randomY), 'snitch');
             this.spr_snitch.scale.set(this.rnd.realInRange(0.3, 0.8));
-            this.speed = this.rnd.between(4000, 9000);
+            this.speed = this.rnd.between(600, 1900);
             this.add.tween(this.spr_snitch).to({ x: 900}, this.speed, Phaser.Easing.Sinusoidal.InOut, true, this.delay, 1000, false);
             this.delay += 200;
         }
-            
 
         // static images
         this.spr_img = this.add.sprite(this.world.centerX, this.world.centerY-100, 'referee');
@@ -43,17 +42,6 @@ ProEvolutionQuidditch.introState.prototype = {
         this.spaceKey = this.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
         this.input.keyboard.addKeyCapture([Phaser.Keyboard.SPACEBAR]);
 
-        // 3 snitches in the front of the screen (right to left)
-        this.delay = 0;
-        for (var i = 0; i < 3; i++)
-        {
-            this.spr_snitch = this.add.sprite(850, -175 + (this.world.randomY), 'snitch');
-            this.spr_snitch.scale.set(this.rnd.realInRange(0.5, 0.9));
-            this.speed = this.rnd.between(4000, 7000);
-            this.add.tween(this.spr_snitch).to({ x: -150}, this.speed, Phaser.Easing.Sinusoidal.InOut, true, this.delay, 1000, false);
-            this.delay += 200;
-        }
-        
     },
 
     update: function () {
