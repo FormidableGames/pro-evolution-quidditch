2a47459146ceefaafcb5340e1a7ddecb919e9ee2
svalverdet <32246322+svalverdet@users.noreply.github.com>
2018-10-29 18:31:53 +0100

    Intro ampliado

--- src/states/intro.js
+++ src/states/intro.js
@@ -42,6 +42,55 @@ ProEvolutionQuidditch.introState.prototype = {
         this.spaceKey = this.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
         this.input.keyboard.addKeyCapture([Phaser.Keyboard.SPACEBAR]);
 
+        game.time.events.add(Phaser.Timer.SECOND * 2.5, this.ampliacion, this);
+    },
+
+    ampliacion: function () {
+        this.spr_img.destroy();
+        this.spr_text.destroy();
+
+        this.spr_griff;
+        this.spr_slyth;
+        this.spr_vs;
+        this.spr_harry;
+        this.spr_draco;
+
+        if (game.one_player) {
+            this.spr_griff = this.add.sprite(this.world.centerX, this.world.centerY - 150, 'griffindor');
+            this.spr_griff.anchor.set(0.5);
+            this.spr_harry = this.add.sprite(-150, this.world.centerY + 45, 'harry');
+            this.spr_harry.scale.setTo(0.65);
+
+            this.tween1 = game.add.tween(this.spr_harry).to({ x: this.world.right + 850 }, 2000, Phaser.Easing.Linear.None, true);
+
+            this.tween1.onComplete.add(function () {
+                this.state.start('levelState');
+            }, this);
+        } else {
+
+            this.spr_griff = this.add.sprite(this.world.centerX-175, this.world.centerY - 150, 'griffindor');
+            this.spr_griff.anchor.set(0.5);
+            this.spr_slyth = this.add.sprite(this.world.centerX+175, this.world.centerY - 150, 'slytherin');
+            this.spr_slyth.anchor.set(0.5);
+            this.spr_vs = this.add.sprite(this.world.centerX, this.world.centerY - 150, 'vs');
+            this.spr_vs.anchor.set(0.5);
+
+            this.spr_harry = this.add.sprite(-150, this.world.centerY + 45, 'harry');
+            this.spr_harry.scale.setTo(0.65);
+            this.spr_harry.anchor.set(0.5);
+            this.spr_draco = this.add.sprite(game.world.right + 150, this.world.centerY + 45, 'draco');
+            this.spr_draco.scale.setTo(-0.65, 0.65);
+            this.spr_draco.anchor.set(0.5);
+
+            this.tween1 = game.add.tween(this.spr_harry).to({ x: game.world.centerX-175 }, 2000, Phaser.Easing.Linear.None, true);
+            this.tween1 = game.add.tween(this.spr_draco).to({ x: game.world.centerX + 175 }, 2000, Phaser.Easing.Linear.None, true);
+
+            this.tween1.onComplete.add(function () {
+                game.time.events.add(Phaser.Timer.SECOND * 2, function () { this.state.start('levelState'); }, this);
+            }, this);
+
+        }
+
     },
 
     update: function () {
